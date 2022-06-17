74eba71f8a0783d30eb0dda056235180b838784b
kissmark_rivas <rivaskissmark@gmail.com>
2022-06-17 02:59:32 -0500

    initialize controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_11_220954) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_180152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_11_220954) do
     t.string "cover"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_companies_on_name", unique: true
   end
 
   create_table "critics", force: :cascade do |t|
@@ -30,6 +31,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_11_220954) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "criticable_type", null: false
+    t.bigint "criticable_id", null: false
+    t.index ["criticable_type", "criticable_id"], name: "index_critics_on_criticable"
     t.index ["user_id"], name: "index_critics_on_user_id"
   end
 
@@ -37,11 +41,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_11_220954) do
     t.string "name"
     t.text "summary"
     t.date "release_date"
-    t.integer "category"
+    t.integer "category", default: 0
     t.decimal "rating"
     t.string "cover"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "parent_id"
+    t.index ["name"], name: "index_games_on_name", unique: true
+    t.index ["parent_id"], name: "index_games_on_parent_id"
   end
 
   create_table "games_genres", id: false, force: :cascade do |t|
@@ -58,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_11_220954) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_genres_on_name", unique: true
   end
 
   create_table "involved_companies", force: :cascade do |t|
@@ -68,26 +76,31 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_11_220954) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["company_id"], name: "index_involved_companies_on_company_id"
+    t.index ["game_id", "company_id"], name: "index_involved_companies_on_game_id_and_company_id", unique: true
     t.index ["game_id"], name: "index_involved_companies_on_game_id"
   end
 
   create_table "platforms", force: :cascade do |t|
     t.string "name"
-    t.integer "category"
+    t.integer "category", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_platforms_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "role"
-    t.integer "critics_coun"
+    t.integer "critics_coun", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "critics", "users"
+  add_foreign_key "games", "games", column: "parent_id"
   add_foreign_key "involved_companies", "companies"
   add_foreign_key "involved_companies", "games"
 end
